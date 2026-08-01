4440b987d2891d62d8f1402cbd937aed53932e17
abdallaboranex-gif <abdalla.boranex@gmail.com>
2026-08-02 00:23:29 +0300

    Add footer rendering function for Streamlit app

--- components/footer.py
+++ components/footer.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+def render_footer():
+    """تذييل الصفحة السفلي"""
+    st.markdown(
+        """
+        <div style='text-align: center; color: #64748B; font-size: 12px;'>
+            منصة البناء المستدام © 2026 - جميع الحقوق محفوظة للشركة المالكة | متوافق مع المدونات الهندسية العراقية
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
